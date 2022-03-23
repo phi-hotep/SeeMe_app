f9d7b2a29af4db41f99c87772d505d8e404fb359
phi-hotep <huguesarnold@gmail.com>
2022-03-23 19:52:34 +0100

    Center SeeMe title

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -14,6 +14,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
         title: const Text('SeeMe'),
         actions: [
