c8d40fc242e8b8a2533e2ba7ebb3e47110fd839d
phi-hotep <huguesarnold@gmail.com>
2022-04-14 20:46:12 +0100

    Adding mock infos

--- lib/screens/drawer_screens/settings_profile.dart
+++ lib/screens/drawer_screens/settings_profile.dart
@@ -5,6 +5,48 @@ class SettingsProfileInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return SafeArea(
+      child: Scaffold(
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => [
+            SliverAppBar(
+              expandedHeight: 240,
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.edit,
+                    size: 25,
+                  ),
+                )
+              ],
+              flexibleSpace: const FlexibleSpaceBar(
+                background: Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/images/femme5.jpg',
+                    ),
+                    radius: 50,
+                  ),
+                ),
+              ),
+              floating: true,
+              snap: true,
+              pinned: true,
+            )
+          ],
+          body: ListView.separated(
+              itemBuilder: (context, index) => ListTile(
+                    title: Text('Item $index'),
+                  ),
+              separatorBuilder: (context, index) => const SizedBox(
+                    height: 20,
+                  ),
+              itemCount: 30),
+        ),
+      ),
+    );
   }
 }
