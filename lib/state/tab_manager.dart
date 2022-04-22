5e795d370fd4c3576a21e09ec7c6bc5bbbfa8696
phi-hotep <huguesarnold@gmail.com>
2022-04-22 18:07:34 +0100

    make selected variable private

--- lib/state/tab_manager.dart
+++ lib/state/tab_manager.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TabManager extends ChangeNotifier {
-  int selectedTab = 0;
+  int _selectedTab = 0;
+
+  int get selectedTab => _selectedTab;
 
   void goToTab(int index) {
-    selectedTab = index;
+    _selectedTab = index;
     notifyListeners();
   }
 }
