7930d26a8656d0b858ef00c6f801692b9ec51745
phi-hotep <huguesarnold@gmail.com>
2022-04-22 18:08:39 +0100

    handle theme only

--- lib/state/theme_manager.dart
+++ lib/state/theme_manager.dart
@@ -2,19 +2,11 @@ import 'package:flutter/material.dart';
 
 class ThemeManager extends ChangeNotifier {
   bool _theme = false;
-  bool _isSwitchToBusinessMode = false;
 
   bool get getTheme => _theme;
-  bool get isSwitchToBusinessMode => _isSwitchToBusinessMode;
 
   void changeTheme() {
     _theme = !_theme; // passer du theme light à dark et vice-versa
-    notifyListeners();
-  }
-
-  set switchMode(bool value) {
-    _isSwitchToBusinessMode =
-        value; // Passer du mode simple au mode business et vice-versa
     notifyListeners();
   }
 }
