1bc73aba8feeab2d6e4636ea37c9deb5662eff2b
phi-hotep <huguesarnold@gmail.com>
2022-04-22 18:13:46 +0100

    New file to handle all app pages

---
+++ lib/models/seeme_pages.dart
@@ -0,0 +1,22 @@
+class SeeMePages {
+  // Ici les pages qui se trouvent dans le dossier /screens
+
+  static String home = 'screens/home';
+  static String shop = 'screens/shop';
+  static String ads = 'screens/ads';
+  static String ambassador = 'screens/ambassador';
+  static String drawer = 'screens/drawer';
+
+  // Ici les pages qui se trouvent dans le dossier /screens/drawer_screens
+
+  static String help = 'screens/drawer_screens/help';
+  static String inviteFriend = 'screens/drawer_screens/invite_friend';
+  static String myActivity = 'screens/drawer_screens/my_activity';
+  static String myCredits = 'screens/drawer_screens/my_credits';
+  static String myStats = 'screens/drawer_screens/my_stats';
+  static String settingsProfileImage =
+      'screens/drawer_screens/settings_profile_image';
+  static String settingsProfile = 'screens/drawer_screens/settings_profile';
+  static String settings = 'screens/drawer_screens/settings';
+  static String shopFollowed = 'screens/drawer_screens/shop_followed';
+}
