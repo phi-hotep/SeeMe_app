d7048e042decfb7cb24c71fa0270cbe4e0068a12
phi-hotep <huguesarnold@gmail.com>
2022-05-18 18:39:20 +0100

    Rename settings folder to Settings_screens

--- lib/models/seeme_pages.dart
+++ lib/models/seeme_pages.dart
@@ -1,8 +1,7 @@
-// update-->
-// change myActivity to myFavorites
+// Ici les liens relatifs à toutes les pages de l'application
 
 class SeeMePages {
-  // Ici les pages qui se trouvent dans le dossier /screens
+  // Ici les pages qui se trouvent dans le dossier /screens -->
 
   static String home = 'screens/home';
   static String shop = 'screens/shop';
@@ -10,7 +9,7 @@ class SeeMePages {
   static String ambassador = 'screens/ambassador';
   static String drawer = 'screens/drawer';
 
-  // Ici les pages qui se trouvent dans le dossier /screens/drawer_screens -->
+  // Ici les pages qui se trouvent dans le dossier /screens/drawer_screens/ -->
   static String settings = 'screens/drawer_screens/settings';
   static String business = 'screens/drawer_screens/business';
   static String myFavorites = 'screens/drawer_screens/my_favorites';
@@ -19,17 +18,22 @@ class SeeMePages {
   static String myCredits = 'screens/drawer_screens/my_credits';
   static String inviteFriend = 'screens/drawer_screens/invite_friend';
 
-// Ici les pages qui se trouvent dans la page settings -->
+// Ici les pages qui se trouvent dans le dossier /screens/drawer_screens/settings_screens/ -->
 
-  static String settingsProfile = 'screens/drawer_screens/settings_profile';
+  static String settingsProfile =
+      'screens/drawer_screens/settings_screens/settings_profile';
   static String settingsProfileImage =
-      'screens/drawer_screens/settings_profile_image';
-  static String settingsAccount = 'screens/drawer_screens/settings_account';
+      'screens/drawer_screens/settings_screens/settings_profile_image';
+  static String settingsAccount =
+      'screens/drawer_screens/settings_screens/settings_account';
   static String settingsNotification =
-      'screens/drawer_screens/settings_notifications';
+      'screens/drawer_screens/settings_screens/settings_notifications';
   static String settingsClearStorage =
-      'screens/drawer_screens/settings_clearStorage';
-  static String settingsLanguage = 'screens/drawer_screens/settings_language';
-  static String settingsUserGuide = 'screens/drawer_screens/settings_userGuide';
-  static String settingsHelp = 'screens/drawer_screens/settings_help';
+      'screens/drawer_screens/settings_screens/settings_clearStorage';
+  static String settingsLanguage =
+      'screens/drawer_screens/settings_screens/settings_language';
+  static String settingsUserGuide =
+      'screens/drawer_screens/settings_screens/settings_userGuide';
+  static String settingsHelp =
+      'screens/drawer_screens/settings_screens/settings_help';
 }
