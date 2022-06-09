4a998f85e03d9618f7a33fdf3798e440c9f08909
phi-hotep <huguesarnold@gmail.com>
2022-06-09 19:52:51 +0100

    Update list of export

--- lib/state/state.dart
+++ lib/state/state.dart
@@ -1,2 +1,10 @@
+// Contenaire regroupant tous les fichiers d'importation se trouvant
+//dans le dossier state
+
+// New update 21/05/2022-->
+// 1) importation files added
+
 export 'tab_manager.dart';
 export 'theme_manager.dart';
+export 'business_manager.dart';
+export 'app_state_manager.dart';
