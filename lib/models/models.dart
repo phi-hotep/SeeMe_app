75cd431c6c20ce5f490e385529c8e58011c96cb1
phi-hotep <huguesarnold@gmail.com>
2022-06-09 19:43:48 +0100

    Update list of export

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,13 @@
+// Page regroupant tous les fichiers models
+
+// New update 21/05/2022-->
+// 1) Update model container with new export
+
+// Exportation des models se trouvant dans le dossier models/-->
+
 export 'femme_model.dart';
 export 'drawer_item_model.dart';
 export 'seeme_pages.dart';
+export 'item_listile_model.dart';
+export 'user_model.dart';
+export 'app_cache.dart';
