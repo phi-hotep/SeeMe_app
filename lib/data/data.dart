7c234f43cc7dc9580517714a9daecaa68363445a
phi-hotep <huguesarnold@gmail.com>
2022-06-09 19:42:25 +0100

    Update list of export

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,1 +1,10 @@
-export 'drawer_item_data.dart';
+// Page regroupant tous les fichiers de données
+
+// New update 21/05/2022-->
+// 1) Update data container with new export
+
+// Exportation des data se trouvant dans le dossier data/-->
+
+export 'drawer_data.dart';
+export 'settings_data.dart';
+export 'settings_profile_info_data.dart';
