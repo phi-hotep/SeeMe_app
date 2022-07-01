9c51f061bb185c765e89d3c4251885820da048fd
phi-hotep <huguesarnold@gmail.com>
2022-07-01 18:42:22 +0100

    Rename 'delete account' from drawer to here

--- lib/screens/drawer_screens/settings_screens/settings_account.dart
+++ lib/screens/drawer_screens/settings_screens/settings_account.dart
@@ -1,7 +1,8 @@
-// update -->
-// New file to display user account
+// La page du compte de l'utilisateur
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:seeme_app/state/state.dart';
 
 import '../../../models/seeme_pages.dart';
 
@@ -21,6 +22,17 @@ class SettingsAccount extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Account'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Ici le theme de l'application est réinitialisé à la valeur par defaut -->
+            Provider.of<ThemeManager>(context, listen: false).initialTheme();
+            // Ici l'utilisateur supprime son compte -->
+            Provider.of<AppStateManager>(context, listen: false).logout();
+          },
+          child: const Text('Logout'),
+        ),
       ),
     );
   }
