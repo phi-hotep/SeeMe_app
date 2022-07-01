d02c28f8c0c42974b19e266df25c611d3d99048c
phi-hotep <huguesarnold@gmail.com>
2022-07-01 18:47:58 +0100

    File to cache user information

--- lib/models/app_cache.dart
+++ lib/models/app_cache.dart
@@ -1,32 +1,57 @@
+// Fichier d'enregistrement des données de connexion etc de l'utilisateur
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppCache {
-  static const kUser = 'user';
-  static const kOnboarding = 'onboarding';
+  static const kUser = 'UserLoggin';
+  static const kOnboarding = 'Onboarding';
+  static const kTheme = 'UserTheme';
+  static const kMode = 'UserMode';
 
-  Future<void> invalidate() async {
+// deconnecte l'utilisateur et le renvoie à la page d'initialisation de l'app -->
+  Future<void> disconnection() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(kUser, false);
-    await prefs.setBool(kOnboarding, false);
+    prefs.setBool(kUser, false);
+    prefs.setBool(kOnboarding, false);
   }
 
-  Future<void> cacheUser() async {
+// Permet de sauvegarder l'état ok de l'utilisateur quand il s'est déjà loggé -->
+  Future<void> cacheUserLogin() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(kUser, true);
+    prefs.setBool(kUser, true);
   }
 
-  Future<void> completeOnboarding() async {
+// Permet de sauvegarder l'état ok de l'utilisateur quand il a déjà traversé l'onboarding -->
+  Future<void> cacheUserOnboarding() async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool(kOnboarding, true);
+    prefs.setBool(kOnboarding, true);
   }
 
+// Permet de récupérer l'état de connexion de l'utilisateur -->
   Future<bool> isUserLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool(kUser) ?? false;
   }
 
-  Future<bool> didCompleteOnboarding() async {
+// Permet de récupérer l'état de l'onboarding de l'utilisateur -->
+  Future<bool> isOnboardingCompleted() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool(kOnboarding) ?? false;
+  }
+
+// Ces deux fonctions enregistrent les préférences de theme de l'utilisateur -->
+  void cacheLightTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(kTheme, false);
+  }
+
+  void cacheDarkTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(kTheme, true);
+  }
+
+  void cacheUserMode(bool val) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool(kMode, val);
   }
 }
