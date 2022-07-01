9acdefca91abf564e5d2c8e3dbb687ef5af3c810
phi-hotep <huguesarnold@gmail.com>
2022-07-01 18:08:33 +0100

    Router and appStateManager implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,57 @@
+// La racine de l'application
+
+// Update: AppStateManager has been connected on the root of app
+
 import 'package:flutter/material.dart';
+import 'package:seeme_app/navigation/router.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'seeme_theme.dart';
-import 'main_home.dart';
 import 'state/state.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const SeeMeApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final _state = AppStateManager(await SharedPreferences.getInstance());
+  _state.initializeApp();
+  runApp(
+    SeeMeApp(
+      appStateManager: _state,
+    ),
+  );
 }
 
 class SeeMeApp extends StatelessWidget {
-  const SeeMeApp({Key? key}) : super(key: key);
+  final AppStateManager appStateManager;
+  const SeeMeApp({Key? key, required this.appStateManager}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final seeMeTheme = SeeMeTheme.dark();
-
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (context) => AppStateManager(),
+          create: (context) => appStateManager,
         ),
         ChangeNotifierProvider(
           create: (context) => ThemeManager(),
         ),
         ChangeNotifierProvider(
           create: (context) => BusinessManager(),
-        )
+        ),
+        Provider<SeeMeRouter>(create: (context) => SeeMeRouter(appStateManager))
       ],
-      child: Consumer<ThemeManager>(builder: (context, _value, child) {
-        return MaterialApp(
-          title: 'SeeMe',
-          debugShowCheckedModeBanner: false,
-          theme: _value.getTheme ? SeeMeTheme.dark() : SeeMeTheme.light(),
-          home: const MainHome(title: 'SeeMe'),
-        );
+      child: Builder(builder: (contextRouter) {
+        final router =
+            Provider.of<SeeMeRouter>(contextRouter, listen: false).router;
+
+        return Consumer<ThemeManager>(builder: (contextRouter, _value, child) {
+          return MaterialApp.router(
+            routeInformationParser: router.routeInformationParser,
+            routerDelegate: router.routerDelegate,
+            title: 'SeeMe',
+            debugShowCheckedModeBanner: false,
+            theme: _value.getTheme ? SeeMeTheme.dark() : SeeMeTheme.light(),
+          );
+        });
       }),
     );
   }
