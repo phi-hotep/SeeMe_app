52e9dc0612f6cabbed54bc263883a0e041c4e92e
phi-hotep <huguesarnold@gmail.com>
2022-07-01 18:13:06 +0100

    GoRouter and animations package implemented

--- lib/main_home.dart
+++ lib/main_home.dart
@@ -1,155 +1,128 @@
 // Page dans laquelle le bottomNavigationBar est implémenté
 
-// Update -->
-// change scale factor when drawer is open
-
 import 'package:flutter/material.dart';
-import 'package:seeme_app/screens/ads.dart';
-import 'package:seeme_app/screens/ambassador.dart';
-import 'package:seeme_app/screens/drawer.dart';
-import 'package:seeme_app/screens/home.dart';
-import 'package:seeme_app/screens/shop.dart';
+import 'package:animations/animations.dart';
 import 'package:provider/provider.dart';
-import 'state/state.dart';
+import 'package:go_router/go_router.dart';
+import 'package:seeme_app/models/seeme_pages.dart';
+import 'package:seeme_app/screens/screens.dart';
+import 'package:seeme_app/state/state.dart';
+
+const _homeKey = PageStorageKey('home_key');
+const _shopKey = PageStorageKey('shop_key');
+const _adsKey = PageStorageKey('ads_key');
+const _ambassadorKey = PageStorageKey('ambassador_key');
 
 class MainHome extends StatefulWidget {
-  const MainHome({Key? key, required this.title}) : super(key: key);
+  static MaterialPage page(String title, int tab) {
+    return MaterialPage(
+      name: SeeMePages.home,
+      key: ValueKey(SeeMePages.home),
+      child: MainHome(title: title, tab: tab),
+    );
+  }
+
+  const MainHome({Key? key, required this.title, required this.tab})
+      : super(key: key);
   final String title;
+  final int tab;
 
   @override
   State<MainHome> createState() => _MainHomeState();
 }
 
 class _MainHomeState extends State<MainHome> {
-  // variables permettant de basculer la page courante
-  // avec les différentes valeurs afin d'afficher la page de menu cachée
-  late double xOffset;
-  late double yOffset;
-  late double scaleFactor;
-  late bool isDrawerOpen;
-  bool isDragging = false;
+  late int _selectedTab = widget.tab;
+  var home, shop, ads, ambassador;
+  List page = [];
 
-// Initialiser les la fonction closeDrawer
   @override
   void initState() {
     super.initState();
-    closeDrawer();
+    home = Home(key: _homeKey, drawerOpen: openDrawer);
+    shop = Shop(key: _shopKey, drawerOpen: openDrawer);
+    ads = Adverts(key: _adsKey, drawerOpen: openDrawer);
+    ambassador = Ambassador(key: _ambassadorKey, drawerOpen: openDrawer);
+    page = [home, shop, ads, ambassador];
   }
 
-  // Methode permettant d'ouvrir le menu
+  // Methode permettant d'ouvrir le menu (l'espace de l'utilisateur) -->
   void openDrawer() {
-    setState(() {
-      xOffset = 360;
-      yOffset = 0;
-      scaleFactor = 0.2;
-      isDrawerOpen = true;
-    });
+    final currentTab = widget.tab.toString();
+    context.goNamed(SeeMePages.drawer, params: {'tab': currentTab});
   }
-
-// Methode permettant de fermer le menu
-  void closeDrawer() {
-    setState(() {
-      xOffset = 0;
-      yOffset = 0;
-      scaleFactor = 1;
-      isDrawerOpen = false;
-    });
-  }
-
-  static List<Widget> pages = <Widget>[
-    const Home(),
-    const Shop(),
-    const Adverts(),
-    const Ambassador(),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<TabManager>(builder: (context, tabManager, child) {
-      return Stack(
-        children: [
-          buildDrawer(),
-          buildPage(tabManager, context),
-        ],
-      );
-    });
+    return Stack(
+      children: [
+        buildDrawer(),
+        buildPage(context),
+      ],
+    );
   }
 
 // Construire la page principale -->
-  Widget buildPage(TabManager tabManager, BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (isDrawerOpen) {
-          closeDrawer();
-          return false;
-        } else {
-          return true;
-        }
-      },
-      child: GestureDetector(
-        onTap: closeDrawer,
-        onHorizontalDragStart: (details) => isDragging = true,
-        onHorizontalDragUpdate: (details) {
-          if (!isDragging) return;
-          const delta = 3;
-          if (details.delta.dx > delta) {
-            openDrawer();
-          } else if (details.delta.dx < -delta) {
-            closeDrawer();
-          }
-          isDragging = false;
-        },
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 250),
-          transform: Matrix4.translationValues(xOffset, yOffset, 0)
-            ..scale(scaleFactor),
-          child: AbsorbPointer(
-            absorbing: isDrawerOpen,
-            child: SafeArea(
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(isDrawerOpen ? 20 : 0),
-                child: Scaffold(
-                  body: IndexedStack(
-                    index: tabManager.selectedTab,
-                    children: [
-                      Home(
-                        drawerOpen: openDrawer,
-                        isDrawerOpen: isDrawerOpen,
-                      ),
-                      const Shop(),
-                      const Adverts(),
-                      const Ambassador(),
-                    ],
+  Widget buildPage(BuildContext context) {
+    return SafeArea(
+      child: ClipRRect(
+        child: Scaffold(
+          body: PageTransitionSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, primaryAnimation, secondaryAnimation) {
+                return FadeThroughTransition(
+                  animation: primaryAnimation,
+                  secondaryAnimation: secondaryAnimation,
+                  child: child,
+                );
+              },
+              child: page[_selectedTab]),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _selectedTab,
+            onTap: (index) {
+              setState(() {
+                _selectedTab = index;
+                switch (_selectedTab) {
+                  case 0:
+                    context.goNamed(SeeMePages.home, params: {'tab': '0'});
+                    Provider.of<AppStateManager>(context, listen: false)
+                        .goToTab(index);
+                    break;
+                  case 1:
+                    context.goNamed(SeeMePages.home, params: {'tab': '1'});
+                    Provider.of<AppStateManager>(context, listen: false)
+                        .goToTab(index);
+                    break;
+                  case 2:
+                    context.goNamed(SeeMePages.home, params: {'tab': '2'});
+                    Provider.of<AppStateManager>(context, listen: false)
+                        .goToTab(index);
+                    break;
+                  case 3:
+                    context.goNamed(SeeMePages.home, params: {'tab': '3'});
+                    Provider.of<AppStateManager>(context, listen: false)
+                        .goToTab(index);
+                    break;
+                }
+              });
+            },
+            type: BottomNavigationBarType.fixed,
+            backgroundColor:
+                Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+            selectedItemColor:
+                Theme.of(context).textSelectionTheme.selectionColor,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Shop'),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.campaign,
+                    size: 30,
                   ),
-                  bottomNavigationBar: BottomNavigationBar(
-                    currentIndex: tabManager.selectedTab,
-                    onTap: (index) {
-                      tabManager.goToTab(index);
-                    },
-                    type: BottomNavigationBarType.fixed,
-                    backgroundColor: Theme.of(context)
-                        .bottomNavigationBarTheme
-                        .backgroundColor,
-                    selectedItemColor:
-                        Theme.of(context).textSelectionTheme.selectionColor,
-                    items: const <BottomNavigationBarItem>[
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.home), label: 'Home'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.store), label: 'Shop'),
-                      BottomNavigationBarItem(
-                          icon: Icon(
-                            Icons.campaign,
-                            size: 30,
-                          ),
-                          label: 'Ads'),
-                      BottomNavigationBarItem(
-                          icon: Icon(Icons.group), label: 'Ambassador'),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                  label: 'Ads'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.group), label: 'Ambassador'),
+            ],
           ),
         ),
       ),
